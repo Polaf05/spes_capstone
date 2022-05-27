4fd922a326d7ca133cbb31288f18238a6337bc6a
Tenpai10 <pransarbey@gmail.com>
2022-05-28 03:26:23 +0800

    added task assessment

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Bar } from "react-chartjs-2";
 import { DataSet } from "../types/Students";
+import { Chart } from "chart.js";
 
 const BarChart = ({
+  display,
   indexAxis,
   labels,
   datasets,
 }: {
+  display: boolean;
   indexAxis: string;
   labels: string[];
   datasets: DataSet[];
 }) => {
   const dataChart = { labels, datasets };
+  type refAttr = {
+    type: string;
+    arr: any[];
+    str: string;
+  };
   return (
     <Bar
       className="pb-2 w-full h-full"
@@ -31,6 +39,12 @@ const BarChart = ({
             display: true,
             position: "top",
           },
+        },
+        onClick: function (evt, element) {
+          // onClickNot working element null
+          console.log(evt, element);
+          if (element.length > 0) {
+          }
         },
       }}
     />
--- pages/[quarter]/[id]/index.tsx
+++ pages/[quarter]/[id]/index.tsx
@@ -89,13 +89,6 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 const capitalize = (string: string) =>
   string.charAt(0).toUpperCase() + string.slice(1);
 
-const getFuzzyValue = (length: number, arr: number[]) => {
-  const new_arr: number[] = [];
-  arr.forEach((item) => {
-    new_arr.push(Number((item / length).toFixed(1)));
-  });
-  return new_arr;
-};
 const getIndexOfMaxNumber = (arr: any[]) => arr.indexOf(Math.max(...arr));
 const getIndexOfMinNumber = (arr: any[]) => arr.indexOf(Math.min(...arr));
 const addOrdinal = (n: number) =>
@@ -589,11 +582,25 @@ const StudentInfo = ({ quarter, id }: { quarter: number; id: string }) => {
           <div className="grid grid-cols-9 h-fit gap-4">
             <div className="col-span-5 bg-neutral-50 p-4 rounded-xl">
               <BarChart
+                display={true}
                 indexAxis="x"
                 labels={myquar}
                 datasets={quarter_dataset}
               />
-              <p className="text-sm text-neutral-500">Fluctuation:</p>
+              <div className="border-t mt-3 text-sm text-neutral-500">
+                <div className="flex gap-2">
+                  <p>
+                    Fluctuation:{" "}
+                    {student?.quarter_analysis.fluctuation.toFixed(1)}
+                  </p>
+                  {student?.quarter_analysis.plunge_task.length! > 0 && (
+                    <p>Plunged: {student?.quarter_analysis.plunge_task}</p>
+                  )}
+                  {student?.quarter_analysis.surge_task.length! > 0 && (
+                    <p>Surged: {student?.quarter_analysis.surge_task}</p>
+                  )}
+                </div>
+              </div>
             </div>
             {/* Overall Performance Assessment */}
             <div className="col-span-4 h-[65vh] overflow-x-auto px-3">
@@ -769,20 +776,16 @@ const StudentInfo = ({ quarter, id }: { quarter: number; id: string }) => {
                 />
                 <div className="border-t mt-3 text-sm text-neutral-500">
                   <div className="flex justify-between">
-                    <p>Written Works</p>
-                    <div className="flex gap-2">
+                    <div className="flex justify-center items-center gap-2">
+                      <div className="bg-yellow-200 w-5 h-1"></div>
+
                       <p>
                         Fluctuation:{" "}
                         {myStudent.written_tasks_analysis?.fluctuation.toFixed(
                           1
                         )}
                       </p>
-                      <p>
-                        Consistency:{" "}
-                        {myStudent.written_tasks_analysis?.fluctuation.toFixed(
-                          1
-                        )}
-                      </p>
+
                       {myStudent.written_tasks_analysis?.plunge_task.length! >
                         0 && (
                         <p>
@@ -799,32 +802,31 @@ const StudentInfo = ({ quarter, id }: { quarter: number; id: string }) => {
                     </div>
                   </div>
                   <div className="flex justify-between">
-                    <p>Performance Tasks</p>
-                    <div className="flex gap-2">
+                    <div className="flex justify-center items-center gap-2">
+                      <div className="bg-ocean-200 w-5 h-1"></div>
                       <p>
                         Fluctuation:{" "}
                         {myStudent.performace_tasks_analysis?.fluctuation.toFixed(
                           1
                         )}
                       </p>
-                      <p>
-                        Consistency:{" "}
-                        {myStudent.performace_tasks_analysis?.fluctuation.toFixed(
-                          1
-                        )}
-                      </p>
+
                       {myStudent.performace_tasks_analysis?.plunge_task
                         .length! > 0 && (
                         <p>
                           Plunged:{" "}
-                          {myStudent.performace_tasks_analysis?.plunge_task}
+                          {myStudent.performace_tasks_analysis?.plunge_task.join(
+                            ", "
+                          )}
                         </p>
                       )}
                       {myStudent.performace_tasks_analysis?.surge_task.length! >
                         0 && (
                         <p>
                           Surged:{" "}
-                          {myStudent.performace_tasks_analysis?.surge_task}
+                          {myStudent.performace_tasks_analysis?.surge_task.join(
+                            ", "
+                          )}
                         </p>
                       )}
                     </div>
