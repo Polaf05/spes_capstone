19505a10168707ca5aca8386044e3eb932cdf838
polaf05 <paularpolo1@gmail.com>
2022-06-21 08:49:11 +0800

    fix remarks

--- pages/getting-started.tsx
+++ pages/getting-started.tsx
@@ -14,7 +14,6 @@ import {
 import {
   fetchJson,
   getGradeAfter,
-  getRemarks,
   getSurveyResults,
   getTask,
   getWeighted,
@@ -41,7 +40,7 @@ import {
 import Intro from "../components/sections/Intro";
 import { useJson } from "../hooks/useSetJson";
 import { useRouter } from "next/router";
-import { transmuteGrade } from "../lib/functions/grade_computation";
+import { transmuteGrade, getRemarks } from "../lib/functions/grade_computation";
 
 const INITIAL_MESSAGE =
   "An error message will appear here if there is problem with your file";
@@ -289,7 +288,7 @@ const gettingStarted = (user: any) => {
                           grade_before: item[34],
                           diff: parseFloat((grade_after - item[35]).toFixed(1)),
                           grade_after: final_grade.satisfaction,
-                          remarks: getRemarks(transmuteGrade(item[34])),
+                          remarks: getRemarks(item[34]),
                           remarks_fuzzy: final_grade.remarks,
                           written_works: written_works,
                           performance_tasks: performace_works,
